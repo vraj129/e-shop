695baf3e653e59afb952304b4701c5bd50e8608e
vraj129 <vrajpatelv@protonmail.com>
2023-12-18 09:38:20 +0530

    -- Category and product schema, CRUD operation api of Category

--- backend/routers/category_router.js
+++ backend/routers/category_router.js
@@ -2,8 +2,62 @@ const express = require("express");
 const router = express.Router();
 const Category = require("../models/category");
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  const category = await Category.find();
+  if (!category) {
+    return res.status(500).json({
+      data: [],
+      success: false,
+    });
+  }
+  return res.status(200).json({
+    data: category,
+    success: true,
+  });
+});
 
-router.post("/", async (req, res) => {});
+router.post("/", async (req, res) => {
+  try {
+    let category = new Category({
+      name: req.body.name,
+      color: req.body.color,
+      icon: req.body.icon,
+    });
+    category = await category.save();
+    if (!category) {
+      return res.status(404).send("The category cannot be created");
+    }
+    return res.send(category);
+  } catch (err) {
+    console.log(err);
+    return res
+      .status(500)
+      .json({ message: "Error fetching data", success: false });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  const categoryId = req.params.id;
+  Category.findByIdAndDelete(categoryId)
+    .then((value) => {
+      if (value) {
+        return res.send({
+          success: true,
+          message: "The category is deleted successfully",
+        });
+      } else {
+        res.status(404).json({
+          success: false,
+          message: "Category not found!",
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      return res
+        .status(400)
+        .json({ message: "Error doing operation", success: false });
+    });
+});
 
 module.exports = router;
