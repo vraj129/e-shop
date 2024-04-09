98f4d08454196c122eacc10d8c8e4c40a42cdaba
vraj129 <vrajpatelv@protonmail.com>
2024-04-09 10:11:15 +0530

    -- User model and routes

--- backend/routers/user_router.js
+++ backend/routers/user_router.js
@@ -2,8 +2,33 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-router.get("/", async (req, res) => {});
+router.get("/", async (req, res) => {
+  const userList = await User.find();
 
-router.post("/", async (req, res) => {});
+  if (!userList) {
+    res.status(500).json({ success: false });
+  }
+  res.send(userList);
+});
+
+router.post("/", async (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: req.body.passwordHash,
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+    street: req.body.street,
+    apartment: req.body.apartment,
+    zip: req.body.zip,
+    city: req.body.city,
+    country: req.body.country,
+  });
+  user = await user.save();
+  if (!user) {
+    return res.status(400).send("The user cannot be registered");
+  }
+  res.send(user);
+});
 
 module.exports = router;
